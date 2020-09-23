4da8a3d562e755a3199d120cdd3c32e3a41ed294
Kimjimin0807 <jm004471@naver.com>
2020-09-23 22:48:07 +0900

    Scaffolding
    Implementation of Random Footprint Generation

---
+++ Platformgame-QSS-master (1)/Platformgame-QSS-master/Platformgame/Assets/Kimjimin/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject _Scaffolding;
+    public float interval;
+    
+    private float distance;
+    private Vector3 dirVec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dirVec = new Vector3(Random.Range(-6.5f, 6.5f), 4.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,1,0);
+        }
+
+        if (transform.position.y > distance)
+        {
+            distance += interval;
+            SpawnScaffolding();
+        }
+    }
+    
+    void SpawnScaffolding()
+    {
+        Instantiate(_Scaffolding, dirVec, Quaternion.identity);
+        dirVec.y += interval;
+        dirVec.x = Random.Range(-6.5f, 6.5f);
+    }
+}
