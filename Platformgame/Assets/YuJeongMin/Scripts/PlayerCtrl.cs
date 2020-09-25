6225b05bb2e66f697e384aa27064ea7809f272aa
유정민 <63496908+tyrosine1153@users.noreply.github.com>
2020-09-25 16:52:02 +0900

    add cameraCtrl

---
+++ Platformgame/Assets/YuJeongMin/Scripts/PlayerCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private float _h, _v;
+    private Transform _tr;
+    public float moveSpeed = 10.0f; 
+    void Start()
+    {
+        _tr = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        _h = Input.GetAxis("Horizontal");
+        _v = Input.GetAxis("Vertical");
+        Vector3 moveDir = (Vector3.right * _h) + (Vector3.up * _v);
+        
+        _tr.Translate(moveDir*moveSpeed*Time.deltaTime,Space.Self);
+    }
+}
