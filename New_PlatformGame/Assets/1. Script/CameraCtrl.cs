68ccf1b482b25c2bbd4c9f3bedf4aa9076e0d5f9
KJG04 <66928355+KJG04@users.noreply.github.com>
2020-10-28 19:50:04 +0900

    1
    1

---
+++ New_PlatformGame/Assets/1. Script/CameraCtrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// written by Yu Jeongmin
+public class CameraCtrl : MonoBehaviour
+{
+    private Transform _transform;
+    private Transform _objectToFollowTransform;
+    private float _followObjectY;
+
+    public GameObject objectToFollow;
+    public float moveSpeed = 3.0f;
+
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+        _objectToFollowTransform = objectToFollow.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //지정한 오브젝트의 Y값을 따라가도록 함
+        _followObjectY = _objectToFollowTransform.position.y;
+        _transform.position = Vector3.Lerp(_transform.position, new Vector3(_transform.position.x, _followObjectY,
+            -10), moveSpeed * Time.deltaTime);
+
+        if (objectToFollow == null)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        //게임 끝내기
+    }
+}
