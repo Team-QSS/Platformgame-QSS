29fb549b7da9fd9b9b6ed7a1e9a7fba9e6d8607f
tyrosine1153 <tyrosine1153@gmail.com>
2020-11-20 23:11:50 +0900

    Create Scene
    타이틀, 대기화면 배치

---
+++ New_PlatformGame/Assets/1. Script/UIFunction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunction : MonoBehaviour
+{
+    public void GoBack()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+    }
+}
